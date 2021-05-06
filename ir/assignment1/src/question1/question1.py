8eb171cd9ea39922249339a9b2ce98d7b3d47465
jhabarsingh <jhabarsinghbhati23@gmail.com>
2021-05-06 16:04:40 +0530

    changed question1

--- ir/assignment1/src/question1/question1.py
+++ ir/assignment1/src/question1/question1.py
@@ -2,12 +2,18 @@ import io
 import re
 import os
 import sys 
+import string
 import unicodedata
 import nltk
 from nltk.corpus import stopwords 
 import unicodedata
 from nltk.tokenize import word_tokenize 
 from nltk.stem import PorterStemmer
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
+lemmatizer = WordNetLemmatizer()
+
+nltk.download('wordnet')
 nltk.download('stopwords')
 
 DEBUG = False
@@ -22,11 +28,17 @@ class DataCleaner():
 	
 	def removeSpaces(self, data):
 		return re.sub(' +', ' ', data)
+	
 
-
+	def removeNumPun(self, data):
+		data = re.sub(r'\d+', '', data)
+		translator = str.maketrans('', '', string.punctuation)
+		return data.translate(translator)
+	
 	def replaceNewline(self, data):
 		return data.replace('\\n', '\n')
 
+	
 	def replaceReturnCarrige(self, data):
 		return data.replace('\\r', '')
 
@@ -62,26 +74,50 @@ class DataCleaner():
     			newData += ps.stem(w) + " "
 		return newData
 
+	def lemmatize(self):
+		lm = WordNetLemmatizer()
+		words = self.data.split()
+		newData = ""
+		for w in words:
+			newData += lm.lemmatize(w) + " "
+		return newData
+
+		
+	def removeSingleLetter(self):
+		data = self.data.split()
+		temp = ""
+		for word in data:
+			if len(word) > 1:
+				temp += word + " "
+		return temp
+
+	
 	def cleanData(self):
 		self.data = self.removeAccent()
 		self.data = self.removestopWord()
+		self.data = self.removeNumPun(self.data)
 		self.data = self.stemming()
-
+		self.data = self.lemmatize()
+		self.data = self.removestopWord()
+		self.data = self.removeSingleLetter()
 		return self.data
 
 	def createFile(self, data):
 		filename =self.filename.split('.')[0] + "_output." + self.filename.split('.')[1]
 		with open(filename, "w+") as wfile:
 			wfile.write(data)
-
+		os.system(f'mv {filename} outputs')	
 		return True
 
-	
+	def countWords(self, input, output):
+		return len(input.split()) - len(output.split())	
 		
+
 if __name__ == "__main__":
 	filename = sys.argv[1]
 	datacleaner = DataCleaner(filename)
 	datacleaner.assignData()
+	input = datacleaner.getText()
 	if DEBUG:
 		print(datacleaner.getText())
 	
@@ -89,3 +125,4 @@ if __name__ == "__main__":
 	changedData = datacleaner.cleanData()
 
 	datacleaner.createFile(changedData)
+	print(f"File words difference {datacleaner.countWords(input, changedData)}")
